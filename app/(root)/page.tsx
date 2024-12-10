e34f77934265fa159a83bee2bc64fac1d88de746
scrjy <sjyplus123@naver.com>
2024-12-10 12:07:30 +0900

    06. setup drizzle orm and postgres database

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,11 @@
-import { Button } from '@/components/ui/button'
+import ProductList from '@/components/shared/product/product-list'
+import sampleData from '@/lib/sample-data'
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-      <button className="bg-red-300 px-4 py-2 rounded-md">버튼</button>
-      <br />
-      <Button>Button</Button>
-      <br />
-      <Button variant={'destructive'}>Button-destructive</Button>
-      <Button variant={'destructive'} size={'lg'}>
-        Button-destructive-lg
-      </Button>
-      <Button variant={'secondary'} size={'sm'}>
-        Button-secondary-sm
-      </Button>
+    <div className="space-y-8">
+      <h2 className="h2-bold">Latest Products</h2>
+      <ProductList data={sampleData.products} />
     </div>
   )
 }
